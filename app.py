43bf83057363d3c58175dac16333f695a606da6b
salma reda <100859819+salmareda104@users.noreply.github.com>
2023-05-18 22:17:03 +0300

    app

---
+++ app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+import pickle as pkl
+
+
+st.set_page_config(
+    page_title="Zomato App",layout="centered",initial_sidebar_state="expanded")
+
+st.title('Zomato App')
+ # front end elements of the web page 
+html_temp = """ 
+    <div style ="background-color:pink;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Predicting success of new restaurants</h1> 
+    </div> 
+    """
+      
+# display the front end aspect
+st.markdown(html_temp, unsafe_allow_html = True) 
+st.subheader('by Salma Reda ')
+
+#take input from user     
+# following lines create boxes in which user can enter data required to make prediction
+
+location = st.selectbox('Name of Location', ('Banashankari', 'Basavanagudi', 'Mysore Road', 'Jayanagar',
+       'Kumaraswamy Layout', 'Rajarajeshwari Nagar', 'Vijay Nagar',
+       'Uttarahalli', 'JP Nagar', 'South Bangalore', 'City Market',
+       'Bannerghatta Road', 'BTM', 'Kanakapura Road', 'Bommanahalli',
+       'Electronic City', 'Sarjapur Road', 'Wilson Garden',
+       'Shanti Nagar', 'Koramangala 5th Block', 'Richmond Road', 'HSR',
+       'Koramangala 7th Block', 'Bellandur', 'Marathahalli', 'Whitefield',
+       'East Bangalore', 'Old Airport Road', 'Indiranagar',
+       'Koramangala 1st Block', 'Frazer Town', 'MG Road', 'Brigade Road',
+       'Lavelle Road', 'Church Street', 'Ulsoor', 'Residency Road',
+       'Shivajinagar', 'Infantry Road', 'St. Marks Road',
+       'Cunningham Road', 'Race Course Road', 'Commercial Street',
+       'Vasanth Nagar', 'Domlur', 'Koramangala 8th Block', 'Ejipura',
+       'Jeevan Bhima Nagar', 'Old Madras Road', 'Seshadripuram',
+       'Kammanahalli', 'Koramangala 6th Block', 'Majestic',
+       'Langford Town', 'Central Bangalore', 'Brookefield',
+       'ITPL Main Road, Whitefield', 'Varthur Main Road, Whitefield',
+       'Koramangala 2nd Block', 'Koramangala 3rd Block',
+       'Koramangala 4th Block', 'Koramangala', 'Hosur Road', 'RT Nagar',
+       'Banaswadi', 'North Bangalore', 'Nagawara', 'Hennur',
+       'Kalyan Nagar', 'HBR Layout', 'Rammurthy Nagar', 'Thippasandra',
+       'CV Raman Nagar', 'Kaggadasapura', 'Kengeri', 'Sankey Road',
+       'Malleshwaram', 'Sanjay Nagar', 'Sadashiv Nagar',
+       'Basaveshwara Nagar', 'Rajajinagar', 'Yeshwantpur', 'New BEL Road',
+       'West Bangalore', 'Magadi Road', 'Yelahanka', 'Sahakara Nagar',
+       'Jalahalli', 'Hebbal', 'Nagarbhavi', 'Peenya', 'KR Puram'))
+
+type_of_name = st.multiselect('Type', ('Restaurant', 'Cafe', 'Hotel'))
+
+
+rest_type_ge = st.multiselect('type of restaurant',('Casual Dining', 'Cafe', 'Quick Bites', 'Delivery', 'Mess',
+       'Dessert Parlor', 'Bakery', 'Pub', 'Fine Dining', 'Beverage Shop',
+       'Sweet Shop', 'Bar', 'Kiosk', 'Food Truck', 'Microbrewery',
+       'Lounge', 'Food Court', 'Dhaba', 'Club', 'Confectionery',
+       'Bhojanalya'))
+
+
+#options 
+st.subheader('What are the services that you will provide? ')
+
+online_order = st.radio("Online ordering is available?: ", ('yes', 'no'))
+book_table = st.radio('Book Table is available',("yes","no")) 
+
+
+# Transform selected options to numerical values
+online_order_value = 1 if online_order == 'yes' else 0
+book_table_value = 1 if book_table == 'yes' else 0
+
+cost=st.slider('Cost For Two',1,10000,100)
+
+count_cuisines = st.slider("Number Of Cuisines ",1,8)
+
+
+st.sidebar.subheader("About App")
+
+st.sidebar.info("This web app is helps you to find out whether your restaurant is going to successed or not.")
+st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether your restaurant is going to successed or not")
+st.sidebar.info("Don't forget to rate this app")
+
+
+
+feedback = st.sidebar.slider('How much would you rate this app?',min_value=0,max_value=5,step=1)
+if feedback:
+    st.header("Thank you for rating the app!")
+    st.info("Caution: This is just a prediction.") 
+
+
+
+df_new = pd.DataFrame ({'location': [location], 'type_of_name':[type_of_name], "rest_type_ge": [rest_type_ge], 'online_order': [online_order_value], 'book_table':[book_table_value], 'cost': [cost], 'count_cuisines': [count_cuisines]})
+
+
+# load transformer
+trans = pkl.load(open('zomato_transformer.pkl', 'rb'))
+#apply transformer on inputs
+x_new = transformer.transform (df_new)
+
+# load model                      
+loaded_model = pkl.load(open('zomato.pkl', 'rb'))
+
+
+#predict the output
+predictx= loaded_model.predict(x_new)[0]
+
+
+if st.button("Predict"):
+    if pred[0] == 0:
+        st.error('Warning! this restaurant is not probably going to succeed!')
+    else:
+        st.success('wow, good luck with this successeful resaurant!')
+    
+#predict output
+success_proba= model.predict_proba(X_new)[0][1]*100
