8c2a96992c999c0367934b0b51e468058857ea53
salma reda <100859819+salmareda104@users.noreply.github.com>
2023-05-19 00:53:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ transformer = pkl.load(open(decompressed_file, 'rb'))
 st.success(f"File '{input_file}' has been decompressed to '{decompressed_file}'")
 
 # apply transformer on inputs
-x_new = transformer.transform (df_new)
+x_new = transformer.transform(pd.DataFrame(df_new))
 #transformed_data = transformer.transform(df_new)
 
 
